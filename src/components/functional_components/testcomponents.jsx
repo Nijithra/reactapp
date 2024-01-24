74924048771c7f2f6265873aaf9e0863a8ad5dce
Nijithra <nijithragk@gmail.com>
2024-01-24 16:01:48 +0530

    This is my react commit

---
+++ src/components/functional_components/testcomponents.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//import { ReactDOM } from "react";
+import TestClassComp from "../class_components/testClassComponent";
+function TestComponent(){
+    return(
+        <div>
+            <TestClassComp/>
+            <h1>This is a test Functional Component</h1>
+            <p>Hi you can easily learn react</p>
+        </div>
+
+    )
+}
+export default TestComponent;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// focus.render(<TestComponent/>)
+
