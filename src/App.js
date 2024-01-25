33be3d7fd1cb5e915f047c9b2a4e38675cb326b4
Nijithra <nijithragk@gmail.com>
2024-01-25 12:13:00 +0530

    This is my react commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from './components/functional_components/testcomponents';
-
+//import TestComponent from './components/functional_components/testcomponents';
+import PropsComponent from './components/functional_components/PropsComponent';
+import StateComponent from './components/class_components/StateComponent';
 
 function App() {
   return (
     <div className="App">
-      <TestComponent/>
+      <PropsComponent name="Nijithra" course="Mern"/>
+      
       <header className="App-header">
-      <TestComponent/>
+    <StateComponent/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './components/class_components/LoginComponent';
+//import Login from './components/class_components/LoginComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    
-    <Login></Login>
+   <App/>
+   
  
 );
 
