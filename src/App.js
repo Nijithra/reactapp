a8a5dcf97f13b0152b149e41283454e4fcad7940
Nijithra <nijithragk@gmail.com>
2024-01-25 16:09:41 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 //import TestComponent from './components/functional_components/testcomponents';
-import PropsComponent from './components/functional_components/PropsComponent';
-import StateComponent from './components/class_components/StateComponent';
+//import PropsComponent from './components/functional_components/PropsComponent';
+//import StateComponent from './components/class_components/StateComponent';
+import NavBar from './components/functional_components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/functional_components/About';
+import Experiance from './components/functional_components/Experiance';
+import Login from './components/functional_components/Login';
+import Home from './components/functional_components/Home';
 
 function App() {
   return (
     <div className="App">
-      <PropsComponent name="Nijithra" course="Mern"/>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={}></Route>
+        <Route path="/Home" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/Experiance" element={<Experiance/>}></Route> 
+        <Route path="/Login" element={<Login/>}></Route>
+      </Routes>
+     
+      </BrowserRouter>
+      
+      {/* <PropsComponent name="Nijithra" course="Mern"/> */}
       
       <header className="App-header">
-    <StateComponent/>
+    {/* <StateComponent/> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
